e7824d599faeda88654aee915025419dbeb30f54
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-16 20:43:03 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,13 @@
 class Pokemon
   
-  def self.initialize
+  attr_accessor :name, :type, :db 
+  attr_reader :db
+  
+  def self.initialize(name, type, db, id=NIL)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
   end 
   
   def self.save
