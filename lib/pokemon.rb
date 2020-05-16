aeeae758fae3c051cf4dc8b79240428e8462ba68
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-16 20:47:08 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,7 @@ class Pokemon
         SQL
         DB[:conn].execute(sql, self.name, self.type, self.db)
 
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
     
   end 
   
