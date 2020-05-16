ab0057fdc23451e9b187a9c8bde69965616e970e
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-16 20:56:18 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,41 +1,21 @@
 class Pokemon
-  
-  attr_accessor :name, :type, :db 
-  attr_reader :db
-  
-  def initialize(name, type, db, id=NIL)
-    @name = name
-    @type = type
-    @db = db
-    @id = id
-  end 
-  
-  def save
-     sql = <<-SQL
-        INSERT INTO pokemon (name, type, db)
-        VALUES (?, ?, ?)
-        SQL
-        DB[:conn].execute(sql, self.name, self.type, self.db)
+  attr_accessor :name, :type, :db, :id, :hp
 
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
-    
-  end 
-  
-  def find(id)
-    sqp = <<-SQL
-    SELECT * FROM pokemon
-    WHERE id = ?
-    LIMIT 1 
-    SQL
-    
-    DB[:conn].execute(sql, id).map do |row|
-      self.new_from_db(row)
-    end.first
-  end 
-  
-   def self.new_from_db(row)
-    pokemon = self.new(row[1], row[2], row[0])
-    pokemon
+  def initialize(keywords)
+
   end
-  
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+  def self.find(num, db)
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
+    new_pokemon = self.new(pokemon)
+    new_pokemon.id = pokemon[0][0]
+    new_pokemon.name = pokemon[0][1]
+    new_pokemon.type = pokemon[0][2]
+    new_pokemon.hp = pokemon[0][3]
+    return new_pokemon
+  end
 end
